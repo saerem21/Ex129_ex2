c216e2cdd0f2cfe41cfd44d17dac42a72c22e973
saerem21 <nemessisdds>
2014-01-06 13:35:13 +0900

    액션시트 입니다.
    129page

--- Ex129_ex2/ViewController.m
+++ Ex129_ex2/ViewController.m
@@ -8,11 +8,40 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIActionSheetDelegate>
 
 @end
 
 @implementation ViewController
+{
+    UIActionSheet *sheet2;
+    int count;
+}
+- (void)actionSheet:(UIActionSheet *)actionSheet willDismissWithButtonIndex:(NSInteger)buttonIndex {
+    //버튼 인덱스
+    NSLog(@"selected index %d",buttonIndex);
+}
+
+-(void)willPresentActionSheet:(UIActionSheet *)actionSheet{
+    if(2 == actionSheet.tag){
+        NSString *title = [NSString stringWithFormat:@"%d 번째",count++];
+        actionSheet.title =title;
+    }
+}
+
+- (IBAction)basicActionsheet:(id)sender {
+    UIActionSheet *sheet = [[UIActionSheet alloc] initWithTitle:@"name" delegate:self cancelButtonTitle:@"cancel" destructiveButtonTitle:@"중요" otherButtonTitles:@"1",@"2",@"3", nil];
+    sheet.tag = 1;
+    [sheet showInView:self.view];
+}
+- (IBAction)actionCount:(id)sender {
+    if(nil == sheet2){
+        sheet2 = [[UIActionSheet alloc] initWithTitle:@"title" delegate:self cancelButtonTitle:nil destructiveButtonTitle:nil otherButtonTitles:@"1",@"2", nil];
+        sheet2.tag = 2;
+    }
+    [sheet2 showInView:self.view];
+}
+
 
 - (void)viewDidLoad
 {
